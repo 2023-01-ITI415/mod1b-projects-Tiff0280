5c928594e246ebcfc3914cba55408eec01f1b903
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-02 00:15:06 -0500

    Destroying Basket when Apple id Dropped

--- Assets/01-Apple Picker/Scripts/ApplePicker.cs
+++ Assets/01-Apple Picker/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class ApplePicker : MonoBehaviour
 {
@@ -9,16 +10,23 @@ public class ApplePicker : MonoBehaviour
     public GameObject basketPrefab;
     public int numBaskets = 3;
     public float basketBottomY = -14f;
-    public float basketSpacingY = 2f; 
+    public float basketSpacingY = 2f;
+
+    public List<GameObject> basketList; 
+
+
     // Start is called before the first frame update
     void Start()
     {
+        basketList = new List<GameObject>(); 
+
         for (int i = 0; i < numBaskets; i++)
         {
             GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
             Vector3 pos = Vector3.zero;
             pos.y = basketBottomY + (basketSpacingY * i);
-            tBasketGO.transform.position = pos; 
+            tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         
         }
     }
@@ -37,10 +45,28 @@ public class ApplePicker : MonoBehaviour
 
         }
 
-    } 
 
-        // Update is called once per frame
-        void Update()
+        // Destroy one of the baskets                                      
+
+        // Get the index of the last Basket in basketList
+
+        int basketIndex = basketList.Count - 1;
+
+        // Get a reference to that Basket GameObject
+
+        GameObject tBasketGO = basketList[basketIndex];
+
+        // Remove the Basket from the list and destroy the GameObject
+
+        basketList.RemoveAt(basketIndex);
+
+        Destroy(tBasketGO);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
     {
         
     }
