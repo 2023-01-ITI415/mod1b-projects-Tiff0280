55b2880df2dd46b11a6b0dce675788e58da77ab1
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-08 18:54:38 -0500

    Added code listing 30.3 to Slingshot Script

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    // fields set in the Unity Inspector pane 
+    [Header("Inscribed")]                                                     
+    public GameObject projectilePrefab;
+ 
+    // fields set dynamically
+    [Header("Dynamic")]
     public GameObject launchPoint;
+    public Vector3 launchPos;                                        
+    public GameObject projectile;                                       
+    public bool aimingMode;
 
     void Awake()
     {
         Transform launchPointTrans = transform.Find("LaunchPoint");     
         launchPoint = launchPointTrans.gameObject;
-        launchPoint.SetActive(false);                                
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
 
@@ -29,7 +39,18 @@ public class Slingshot : MonoBehaviour
         launchPoint.SetActive(false);
     }
 
+    void OnMouseDown()
+    {                                                       
+        // The player has pressed the mouse button while over Slingshot
+        aimingMode = true;
+         // Instantiate a Projectile
+        projectile = Instantiate(projectilePrefab) as GameObject;
+         // Start it at the launchPoint
+        projectile.transform.position = launchPos;
+         // Set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;              
+    }
 
 
- 
+
 }
