4d55d995c520cb7f4a0882d7637c8eb3594427cb
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-20 01:41:44 -0500

    Updated FollowCam Settings

--- Assets/03-Prototype1/Scripts/P1FollowCam.cs
+++ Assets/03-Prototype1/Scripts/P1FollowCam.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class P1FollowCam : MonoBehaviour
 {
+    static private P1FollowCam S;   // Another private Singleton
     static public GameObject POI; // The static point of interest          
+
+    public enum eView { none, slingshot, castle, both };
 
     [Header("Inscribed")]
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero; // Vector2.zero is [0,0]
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ; // The desired Z pos of the camera 
+    public eView nextView = eView.slingshot;
 
     void Awake()
     {
+        S = this;
         camZ = this.transform.position.z;
     }
 
@@ -48,5 +54,37 @@ public class P1FollowCam : MonoBehaviour
 
         // Set the orthographicSize of the Camera to keep Ground in view
         Camera.main.orthographicSize = destination.y + 10;
+    }
+
+    public void SwitchView(eView newView)
+    {                                  // f
+        if (newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {                                                 
+            case eView.slingshot:
+            POI = null;
+            nextView = eView.castle;
+            break;
+            case eView.castle:
+            POI = MissionDemolition.GET_CASTLE();                        
+            nextView = eView.both;
+            break;
+            case eView.both:
+            POI = viewBothGO;
+            nextView = eView.slingshot;
+            break;
+        }
+    }
+    public void SwitchView()
+    {                                              
+        SwitchView(eView.none);
+    }
+
+    static public void SWITCH_VIEW(eView newView)
+    {                           // j
+        S.SwitchView(newView);
     }
 }
