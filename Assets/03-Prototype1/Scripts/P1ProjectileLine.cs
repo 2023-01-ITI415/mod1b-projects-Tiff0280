41f6b82dd450706ed6290fd572fc8bac0033e208
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-20 01:05:09 -0500

    Edited ProjectileLine Script & Made into Prefab

--- Assets/03-Prototype1/Scripts/P1ProjectileLine.cs
+++ Assets/03-Prototype1/Scripts/P1ProjectileLine.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
+
 public class P1ProjectileLine : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private LineRenderer _line;
+    private bool _drawing = true;
+    private Projectile _projectile;
+
     void Start()
     {
-        
+        _line = GetComponent<LineRenderer>();
+        _line.positionCount = 1;
+        _line.SetPosition(0, transform.position);
+
+        _projectile = GetComponentInParent<Projectile>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (_drawing)
+        {
+            _line.positionCount++;
+            _line.SetPosition(_line.positionCount - 1, transform.position);
+            // If the Projectile Rigidbody is sleeping, stop drawing
+            if (_projectile != null)
+            {
+                if (!_projectile.awake)
+                {
+                    _drawing = false;
+                    _projectile = null;
+                }
+            }
+        }
     }
 }
