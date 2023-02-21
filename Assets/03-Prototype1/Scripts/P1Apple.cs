84a5e6b74e97e530abb0b56675fee0b4f4401d17
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-21 14:51:05 -0500

    Updated Apple Script

--- Assets/03-Prototype1/Scripts/P1Apple.cs
+++ Assets/03-Prototype1/Scripts/P1Apple.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class P1Apple : MonoBehaviour
 {
+    public static float bottomY = -20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,9 @@ public class P1Apple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);                              
+        }
     }
 }
