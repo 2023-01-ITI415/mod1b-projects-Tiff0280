b9c14d1953323d7cbffcf56ed7da666b13e823aa
Tiff0280 <ty194@scarletmail.rutgers.edu>
2023-02-21 19:12:02 -0500

    Added Score Counter & high Score

---
+++ Assets/03-Prototype1/Scripts/P1Basket.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Basket : MonoBehaviour
+{
+    public P1ScoreCounter scoreCounter;
+
+    void Start()
+    {
+        // Find a GameObject named ScoreCounter in the Scene Hierarchy
+        GameObject scoreGO = GameObject.Find("P1ScoreCounter");      
+        // Get the ScoreCounter (Script) component of scoreGO
+        scoreCounter = scoreGO.GetComponent<P1ScoreCounter>();          
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {                           
+        // Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;                        
+        if (collidedWith.CompareTag("Apple"))
+        {                         
+            Destroy(collidedWith);
+            // Increase the score
+            scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE(scoreCounter.score);
+        }
+    }
+}
